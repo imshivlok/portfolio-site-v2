4e3d7a82dd6c30706c6407952c4e7ff002e433e2
imshivlok <138818719+imshivlok@users.noreply.github.com>
2023-10-29 23:07:20 +0530

    Add files via upload

---
+++ pages/maintenance.js
@@ -0,0 +1,11 @@
+let ham = document.getElementById("hamburger")
+let ul2 = document.getElementById("myLinks")
+
+function myLinks() {
+  if (ul2.style.display=="flex"){
+	  ul2.style.display = "none";
+	  }
+  else{
+	  ul2.style.display="flex";
+	  }
+}
